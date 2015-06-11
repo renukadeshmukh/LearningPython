c0ca782128e99c59c6b4c2c6a16adf7a5ada45a7
Renuka Deshmukh <renud1988@gmail.com>
2015-06-11 10:02:20 -0700

    Calculate and print grade based on input

---
+++ 14. PrintGrade.py
@@ -0,0 +1,30 @@
+# 3.3 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, 
+# print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
+# Score Grade
+# >= 0.9 A
+# >= 0.8 B
+# >= 0.7 C
+# >= 0.6 D
+# < 0.6 F
+# If the user enters a value out of range, print a suitable error message and exit. For the test, 
+# enter a score of 0.85.
+
+input = raw_input("Enter grade value between 0.0 and 1.0")
+
+fVal = float(input)
+
+if fVal > 1.0:
+    print "Error"
+elif fVal >= 0.9 :
+    print "A"
+elif fVal >= 0.8:
+	print "B"
+elif fVal >= 0.7:
+	print "C"
+elif fVal >= 0.6:
+	print "D"
+elif fVal < 0.6:
+	print "F"
+else :
+    print "Error! Input out of range"
+   
