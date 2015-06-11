8e843fec43bd6dded50b0f39f9d625a8b9a29105
Renuka Deshmukh <renud1988@gmail.com>
2015-06-11 09:08:32 -0700

    Try Except in python

---
+++ 12. try-except.py
@@ -0,0 +1,21 @@
+
+str = "Hello"
+
+try :
+	istr = int(str)
+except : 
+	print("Conversion failed")
+	istr = -1
+	
+print istr
+
+
+str = "1234"
+
+try :
+	istr = int(str)
+except : 
+	print("Conversion failed")
+	istr = -1
+	
+print istr
