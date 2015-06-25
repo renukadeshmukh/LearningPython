ca4cc3b6369aab27c11c93be30fb4d38bfe12954
Renuka Deshmukh <renud1988@gmail.com>
2015-06-25 10:34:34 -0700

    combination of if and while loop

--- 19. Loops.py
+++ 19. Loops.py
@@ -6,3 +6,41 @@ while x >= 0:
 	x = x - 1
 
 print "All Done"
+
+for i in [5,4,3,2,1,0] :
+	print "In for loop ", i, "times"
+print ""
+print ""
+
+
+print "Max number is ::"	
+# find the largest number from array
+arr = [54, 76, 48, 40, 7, 98, 24, 18]
+max = arr[0]
+for i in arr : 
+	if i >= max :
+		max = i
+		
+print max
+print ""
+print ""
+
+print "Sum of array numbers is ::"	
+# find the sum of number from array
+sum = 0
+for i in arr : 
+	sum += i
+		
+print sum
+
+print ""
+print ""
+
+print "Numbers greater than 40 are ::"	
+# find the sum of number from array
+for i in arr : 
+	if i > 40 : 
+		print i
+		
+
+		
